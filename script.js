63b072f1d213dde684e94402a19d41e27e554f4d
Samuel Odumu <themainsamuel@gmail.com>
2025-02-02 06:04:13 +0100

    modified time function to update in real-time

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 // get and insert current time in UTC format
-const currentTime = new Date().toUTCString();
-document.getElementById("current-time").innerHTML = currentTime;
+function updateTime() {
+    let time = new Date().toUTCString();
+    // cut out seconds
+    let formattedTime = time.replace(/(:\d{2})\s/, ' ');
+    document.getElementById('current-time').innerText = formattedTime;
+}
+
+setInterval(updateTime, 1000);
+updateTime(); // initial call to prevent delay
