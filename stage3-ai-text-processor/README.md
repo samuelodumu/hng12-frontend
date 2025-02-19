d1f36c1287c8d2d9d4b58c9d104155c3dc7fa74c
Samuel Odumu <themainsamuel@gmail.com>
2025-02-19 04:42:09 +0100

    set up the basics

---
+++ stage3-ai-text-processor/README.md
@@ -0,0 +1,12 @@
+# HNG12 Stage 3 task
+An AI language processor built with React
+
+# [Live link](https://themainsamuel-ai-text-processor-hng12.netlify.app/)
+
+# Features
+- Users can input text, send text and it renders in the output field.
+- Users can summarize text outputs if the output text is more than 150 characters.
+- Users can translate output texts to the different languages listed.
+- Processed results are displayed in the output area after API calls are complete.
+
+# Improvements
---
+++ stage3-ai-text-processor/src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [text, setText] = useState('');
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      if (text.trim() !== '') {
+        sendChat();
+        e.preventDefault();
+      }
+    }
+  };
+
+  const sendChat = () => {
+    const output = document.querySelector('.output-text');
+    output.classList.add('text-end');
+    output.value += `${text}\n`;
+    output.scrollTop = output.scrollHeight
+    setText('');
+  };
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  return (
+    <>
+      <div className='container-fluid'>
+        <header className='mb-5'>
+          <h1 className='display-4'>BrieflyAI</h1>
+        </header>
+        <div className='container'>
+          <div className='output-group'>
+            <textarea
+              className='output-text form-control mb-4 p-3 border-0 rounded-5 fw-medium'
+              rows={12}
+              cols={100}
+              readOnly></textarea>
+          </div>
+        </div>
+        <div className='container'>
+          <div className='input-group'>
+            <textarea
+              rows={4}
+              cols={100}
+              onKeyDown={handleKeyDown}
+              onChange={handleChange}
+              value={text}
+              className='form-control rounded-5 p-3 position-relative input-text'
+              aria-label='With textarea'></textarea>
+            <span className='send-icon fs-5 position-absolute align-self-end text-light px-3 py-2 rounded-4'>
+              <i className='bi bi-send'></i>
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
